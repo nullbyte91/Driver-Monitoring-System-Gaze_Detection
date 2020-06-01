a62374befd1b7b4f50d7feb1b1013d00ecc2aeab
Jegathesan Shanmugam <nullbyte.in@gmail.com>
2020-06-02 02:49:55 +0530

    output_blob assertion removed: Note: when we use facial-landmarks-35-adas the ouput blob different from retail onne

--- core/landmarks_detector.py
+++ core/landmarks_detector.py
@@ -1,19 +1,3 @@
-"""
- Copyright (c) 2018 Intel Corporation
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-"""
-
 import numpy as np
 
 from utils.helper import cut_rois, resize_input
@@ -46,12 +30,6 @@ class LandmarksDetector(Module):
         self.input_blob = next(iter(model.inputs))
         self.output_blob = next(iter(model.outputs))
         self.input_shape = model.inputs[self.input_blob].shape
-
-        assert np.array_equal([1, self.POINTS_NUMBER * 2, 1, 1],
-                              model.outputs[self.output_blob].shape), \
-            "Expected model output shape %s, but got %s" % \
-            ([1, self.POINTS_NUMBER * 2, 1, 1],
-             model.outputs[self.output_blob].shape)
 
     def preprocess(self, frame, rois):
         assert len(frame.shape) == 4, "Frame shape should be [1, c, h, w]"
