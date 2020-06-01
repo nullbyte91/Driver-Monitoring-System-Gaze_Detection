a1f67c712f80a03c0f5e4ab0200daa6fa387dae8
Jegathesan Shanmugam <nullbyte.in@gmail.com>
2020-06-02 02:48:04 +0530

    landmarks model updated to facial-landmarks-35-adas-0002

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -6,7 +6,7 @@ sDownload="/opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader/
 
 fFaceDetection="face-detection-adas-binary-0001"
 fHeadPosition="head-pose-estimation-adas-0001"
-fLandMark="landmarks-regression-retail-0009"
+fLandMark="facial-landmarks-35-adas-0002"
 fGazeEstimation="gaze-estimation-adas-0002"
 
 #Main starts
@@ -16,13 +16,13 @@ mkdir -p ${dModel}
 # Download face-detection-adas-binary-0001
 python3 ${sDownload} --name ${fFaceDetection} -o ${dModel}
 
-# Download face-detection-adas-binary-0001
+# Download head-pose-estimation-adas-0001
 python3 ${sDownload} --name ${fHeadPosition} -o ${dModel}
 
-# Download face-detection-adas-binary-0001
+# Download facial-landmarks-35-adas-0002
 python3 ${sDownload} --name ${fLandMark} -o ${dModel}
 
-# Download face-detection-adas-binary-0001
+# Download gaze-estimation-adas-0002
 python3 ${sDownload} --name ${fGazeEstimation} -o ${dModel}
 
 echo "Download Done"
